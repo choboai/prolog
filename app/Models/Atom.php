b3bf123fe1fc7e1d613e221311467f9d6528e3db
Thibault Six <t.six@guest.uliege.be>
2020-09-27 15:04:48 +0200

    ide-helper config

--- app/Models/Atom.php
+++ app/Models/Atom.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @mixin IdeHelperAtom
+ */
 class Atom extends Model
 {
     use HasFactory;
--- app/Models/PrologQuery.php
+++ app/Models/PrologQuery.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @mixin IdeHelperPrologQuery
+ */
 class PrologQuery extends Model
 {
     use HasFactory;
