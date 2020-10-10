c96d54668b04edbfe0f8da267208fc225bb28da0
Thibault Six <t.six@guest.uliege.be>
2020-10-10 20:31:30 +0200

    ajout de deux tests simples

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,19 +2,37 @@
 
 namespace Tests\Feature;
 
+use App\Models\PrologFile;
+use App\Models\PrologQuery;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testBaseUrl()
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertRedirect(route('programs.index'));
+    }
+
+    public function testProgramsIndex()
+    {
+        $programs = \App\Models\Program::factory(10)
+            ->has(PrologFile::factory()->count(1), 'prolog_files')
+            ->has(PrologQuery::factory()->count(1), 'prolog_queries')
+            ->create();
+
+        $response = $this->get(route('programs.index'));
+        foreach ($programs->pluck('name')->toArray() as $program) {
+            $response->assertSee($program);
+        }
     }
 }
