171ea603c285e36260ce35049c710c5dc42c7913
Thibault Six <t.six@guest.uliege.be>
2020-10-19 03:00:49 +0200

    btn outline selected query

--- resources/views/livewire/programs/show.blade.php
+++ resources/views/livewire/programs/show.blade.php
@@ -89,7 +89,7 @@
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
                                                 </svg>
                                             </button>
-                                            <button x-data @click="evaluate($event)" class="ml-2 flex items-center text-sm py-1 px-1 border border-gray-200 rounded-lg shadow-sm hover:shadow-md cursor-pointer float-right" type="button">
+                                            <button x-data @click="evaluate($event)" class="ml-2 flex items-center text-sm py-1 px-1 border border-gray-200 rounded-lg shadow-sm hover:shadow-md cursor-pointer float-right focus:shadow-outline-green focus:outline-none" type="button">
                                                 <svg class="w-5 h-5 mr-1 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                                                 <span class="pr-1">Execute</span>
                                             </button>
